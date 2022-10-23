1325006c85ecd1391f74ba1a0e703c710dd7c78e
Edgardo Ramos <rhedgardo@gmail.com>
2022-10-23 00:26:12 -0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Minimal_API.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,25 @@
+﻿namespace Minimal_API.Models
+{
+    public class Task
+    {
+        public Guid TaskId { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public string Description { get; set; }
+
+        public Priority TaskPriority { get; set; }
+
+        public DateTime DateTimeCreated { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+    
+    public enum Priority
+    {
+        Low,
+        Mid,
+        High
+    }
+}
